e0a46a3ae4f14d49fe3365548436c112bd6ff137
ilyinalex <elitstudentbook@gmail.com>
2018-03-01 15:28:39 +0200

    add Main and "New branch"

--- ProjectForRepoTest/src/edu/Main.java
+++ ProjectForRepoTest/src/edu/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("New branch");
         System.out.println("PushMaster");
+        System.out.println("Inc");
     }
 }
